f3697c9092e8ecb5277a03c17adb24ff1786bb89
Giovana Morais <giovana.vmorais@gmail.com>
2019-01-26 22:44:54 -0200

    otimizações para o k-means agora o algoritmo redefine o tamanho da imagem, o que faz com que a execução fique muito mais rápida. o tamanho da imagem ser parâmetro também permite que o usuário consiga escolher o quão refinada ele quer que a clusterização seja. pra evitar retrabalho, agora o algoritmo tá salvando os centroides já calculados em arquivos .pickle. ainda preciso ver como fazer pra que o arquivo não seja carregado caso o número de centroides desejados mude.

--- colors.py
+++ colors.py
@@ -1,5 +1,8 @@
+import os
 import math
 import numpy as np
+import pickle as pkl
+
 from PIL import Image
 from sklearn import metrics
 from sklearn.cluster import KMeans
@@ -25,7 +28,18 @@ def distance(x, y, alg="euclidian"):
     return dist 
 
 # dada uma matriz inicial, faz um agrupamento dos pixels mais próximos 
-def get_closest_pixels(img, colors):
+def get_closest_pixels(img, colors=None):
+    if not colors:
+        colors = {
+                    (255, 0  , 0)   : 0,    # red
+                    (255, 127, 0)   : 0,    # orange
+                    (255, 255, 0)   : 0,    # yellow
+                    (0  , 255, 0)   : 0,    # green
+                    (0  , 0  , 255) : 0,    # blue
+                    (75 , 0  , 130) : 0,    # purple
+                    (143, 0  , 255) : 0     # violet
+                  }
+
     # já retorna uma lista com cada item sendo uma tupla do tipo (r, g, b)
     pixels = list(img.getdata())
 
@@ -39,29 +53,61 @@ def get_closest_pixels(img, colors):
 
     print(colors)
 
-def get_clusters(img):
-    n_clusters = 10 
-    pixels = list(img.getdata())
+def get_clusters(image, n_clusters=3, resize_method="nearest", new_size=(50,50)):
+    pkl_file = image.split('.')[0] + ".pickle"
 
-    color_cluster = KMeans(n_clusters).fit(pixels)
-    print(color_cluster.cluster_centers_)
-    return color_cluster.cluster_centers_
+    if os.path.isfile(pkl_file):
+        centroids = load_file(pkl_file)
+    else:
+        img = Image.open(image)
+
+        if resize_method == "nearest":
+            resized = img.resize(new_size, Image.NEAREST)
+        elif resize_method == "bilinear":
+            resized = img.resize(new_size, Image.BILINEAR)
+        elif resize_method == "bicubic":
+            bicubic = img.resize(new_size, Image.BICUBIC)
+        elif resize_method == "lanczos":
+            lanczos = img.resize(new_size, Image.LANCZOS)
+        else:
+            raise(ValueError("{} is a invalid resize method".format(resize_method)))
+
+        pixels = list(resized.getdata())
+
+        color_cluster = KMeans(n_clusters).fit(pixels)
+        centroids = color_cluster.cluster_centers_
+        print(color_cluster.cluster_centers_)
+
+        save_file(pkl_file, centroids)
+    return centroids
 
 
-def show_color(rgb):
+def load_file(file):
+    print("Carregando arquivo {}".format(os.path.basename(file)))
+    with open(file, 'rb') as f:
+        data = pkl.load(f)
+    return data 
+
+
+def save_file(file, data):
+    print("Salvando arquivo {}".format(os.path.basename(file)))
+    with open(file, 'wb') as f:
+        pkl.dump(data, f)
+
+def show_colors(rgb):
     bar = 0
     hex = rgb_to_hex(rgb)
     for i in rgb:
-        plt.bar(bar, 2, color=hex[bar])
+        plt.bar(hex[bar], 2, color=hex[bar])
         bar += 1
 
     plt.show()
 
 def rgb_to_hex(rgb):
     hex = []
-    print("RGB: {}".format(rgb))
     for i in rgb:
         hex.append('#{:02x}{:02x}{:02x}'.format(int(i[0]), int(i[1]), int(i[2])))
 
-    print("HEX {}".format(hex))
+    #print("RGB: {}".format(rgb))
+    #print("HEX {}".format(hex))
     return hex
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 
@@ -6,35 +7,54 @@ from PIL import Image
 from colors import *
 from hist import *
 
+FILE = sys.argv[1]
 
-file = sys.argv[1]
-img = Image.open(file)
+if os.path.isfile(FILE):
+    print("Parâmetro é um arquivo")
+    if not os.path.isabs(FILE):
+        FILE = os.path.join(os.getcwd(), FILE)
+    #img = Image.open(FILE)
+    cluster = get_clusters(FILE, n_clusters=5)
+    print(cluster)
+    show_colors(cluster)
 
-# tabela básica com as cores do arco íris pro primeiro cálculo de distância.
-# vai ser modificada depois pq vai ser gerada a partir da entrada
-colors = {
-            (255, 0  , 0)   : 0,    # red
-            (255, 127, 0)   : 0,    # orange
-            (255, 255, 0)   : 0,    # yellow
-            (0  , 255, 0)   : 0,    # green
-            (0  , 0  , 255) : 0,    # blue
-            (75 , 0  , 130) : 0,    # purple
-            (143, 0  , 255) : 0     # violet
-          }
+    
+elif os.path.isdir(FILE):
+    clusters = []
+    
+    print("Parâmetro é um diretório")
+    if not os.path.isabs(FILE):
+        FILE = os.path.join(os.getcwd(), FILE)
+    for root, dirs, files in os.walk(FILE):
+        for i in files:
+            tmp = {}
+            tmp['id'] = os.path.join(root, i)
+            tmp['clusters'] = get_clusters(os.join(root, i), "nearest", 5)
+            clusters.append(tmp)
+    
+
+    print(clusters)
 
 
 """
-print("Calculando distância na mão")
-init = time.time()
-get_closest_pixels(img, colors)
-print("Tempo de execução: {}".format(time.time()-init))
-"""
-
 print("Calculando distância com o kmeans")
 init = time.time()
-clusters = get_clusters(img)
+clusters = get_clusters(nearest, 5)
 print("Tempo de execução: {}".format(time.time()-init))
 
 show_color(clusters)
 #img.show()
 
+
+Modo 1: só a pasta de imagens é passada
+Gera os arquivos com todos os centróides e depois os agrupa por similaridade
+É um kmeans seguido de um kmeans
+
+Modo 2: uma imagem é passada
+Gera os centroides da imagem. Se tiver uma pasta de imagens, separa as imagens mais parecida
+com a entrada. 
+
+Modo 3: uma ou mais cores são passadas
+Gera as imagens e compara com a cor em questão
+
+"""
